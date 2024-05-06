4a5289d9bd767abed02602793febe5b04308d058
lim-haru <123555071+lim-haru@users.noreply.github.com>
2024-05-06 17:40:15 +0200

    Implemented frontend for QuizStart/QuizEnd

--- src/components/QuizEnd.tsx
+++ src/components/QuizEnd.tsx
@@ -24,13 +24,29 @@ export default function QuizEnd(props: QuizEndProps) {
   const skillLevel: SkillLevel = getSkillLevel(props.score)
 
   return (
-    <div>
-      <h2>Quiz Completato!</h2>
-      <p>
-        Il tuo Punteggio: {props.score}/{props.totalQuestions}
+    <div className="d-flex flex-column align-items-center">
+      <h2 className="mb-4">Quiz Completato!</h2>
+      <p className="fs-4">
+        Il tuo Punteggio:{" "}
+        <b>
+          {props.score}/{props.totalQuestions}
+        </b>
       </p>
-      <p>Livello di Abilità: {skillLevel}</p>
-      <button onClick={props.onRestartQuiz}>Ricomincia</button>
+      <p className="fs-4 mb-5">
+        Livello di Abilità: <b>{skillLevel}</b>
+      </p>
+      <button className="button" onClick={props.onRestartQuiz}>
+        Ricomincia
+        <div className="icon">
+          <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0 0h24v24H0z" fill="none"></path>
+            <path
+              d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
+              fill="currentColor"
+            ></path>
+          </svg>
+        </div>
+      </button>
     </div>
   )
 }
--- src/components/QuizStart.tsx
+++ src/components/QuizStart.tsx
@@ -7,14 +7,25 @@ type onStartQuizProps = {
 
 export default function QuizStart(props: onStartQuizProps) {
   return (
-    <div>
-      <h2>Benvenuto al Quiz sul Turismo Sostenibile!</h2>
-      <p>
+    <div className="d-flex flex-column align-items-center">
+      <h2 className="mb-4">Benvenuto al Quiz sul Turismo Sostenibile!</h2>
+      <p className="fs-5 mb-5 px-0 px-sm-3 px-md-5">
         Questo quiz è progettato per testare le tue conoscenze sul turismo sostenibile e per aiutarti a comprendere l'importanza
         di viaggiare in modo responsabile per preservare l'ambiente e sostenere le comunità locali. Il quiz consiste in{" "}
         {props.totalQuestions} domande a risposta multipla.
       </p>
-      <button onClick={props.onStartQuiz}>Inizia!</button>
+      <button className="button" onClick={props.onStartQuiz}>
+        Inizia!
+        <div className="icon">
+          <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0 0h24v24H0z" fill="none"></path>
+            <path
+              d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
+              fill="currentColor"
+            ></path>
+          </svg>
+        </div>
+      </button>
     </div>
   )
 }
